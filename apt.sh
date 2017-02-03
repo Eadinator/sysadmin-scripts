1c1ecb4b4e02bf6ce4c3d9f484aff255721d9fbc
Eadinator <matthew@tech3.me>
2017-02-03 00:39:06 +0000

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dpkg -l | awk '{print $2}' | egrep 'linux-(image|headers)(-extra)?-[0-9]' | grep -v -f <(sed s/-generic// <(uname -r)) | xargs --no-run-if-empty apt --yes purge
-
 apt update && apt --purge --yes full-upgrade
+
+dpkg -l | awk '{print $2}' | perl -ne 'chomp; print "$1\t$_\n" if ($_ =~ /linux-(?:image|headers)(?:-extra)?-([0-9].*)/)' | sort -V -k1 | perl -ne 'chomp; my ($short, $long) = split(/\t/, $_); if (index(qx/uname -r/, $short) != -1){exit} else{print "$long\n"}' | xargs --no-run-if-empty apt --yes purge
