23a858f60c2a1d1637fe6b763ff140af2f1cadc5
Eadinator <matthew@tech3.me>
2017-02-18 01:24:09 +0000

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -2,4 +2,4 @@
 
 apt update && apt --purge --yes full-upgrade
 
-dpkg -l | awk '{print $2}' | perl -ne 'chomp; print "$1\t$_\n" if ($_ =~ /linux-(?:image|headers)(?:-extra)?-([0-9].*)/)' | sort -V -k1 | perl -ne 'chomp; my ($short, $long) = split(/\t/, $_); if (index(qx/uname -r/, $short) != -1){exit} else{print "$long\n"}' | xargs --no-run-if-empty apt --yes purge
+dpkg -l | awk '{print $2}' | perl -ne 'chomp; print "$1\t$_\n" if ($_ =~ /^linux-(?:(?:signed-)?image|image-extra|headers)-(\d.*)$/)' | sort -V -k1 | perl -ne 'chomp; my ($short, $long) = split(/\t/, $_); if (index(qx/uname -r/, $short) != -1){exit} else{print "$long\n"}' | xargs --no-run-if-empty apt --yes purge
