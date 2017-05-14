1212f19dc736e220333f967cd0cd6ed2db514cb4
Eadinator <matthew@tech3.me>
2017-05-14 22:34:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-AUTOSTART_DIR="~/.config/autostart"
+AUTOSTART_DIR=~/.config/autostart
 GITHUB_URL="https://raw.githubusercontent.com/Eadinator/sysadmin-scripts/master"
 
 sudo apt update
@@ -18,3 +18,4 @@ wget -P $AUTOSTART_DIR $GITHUB_URL/eterm.desktop
 wget -P $AUTOSTART_DIR $GITHUB_URL/startup.sh
 wget -P $AUTOSTART_DIR $GITHUB_URL/bleachbit.sh
 wget -P $AUTOSTART_DIR $GITHUB_URL/apt.sh
+wget -P $AUTOSTART_DIR $GITHUB_URL/filter_pkglist.pl
