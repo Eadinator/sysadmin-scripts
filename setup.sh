602e483054245c560527c82cec9835bbfa3c0967
Eadinator <matthew@tech3.me>
2017-05-14 20:29:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-mkdir -p ~/.config/autostart
+AUTOSTART_DIR="$HOME/.config/autostart"
+GITHUB_URL="https://raw.githubusercontent.com/Eadinator/sysadmin-scripts/master"
 
-wget -P ~/.config/autostart https://raw.githubusercontent.com/Eadinator/sysadmin-scripts/master/eterm.desktop
+sudo apt update
+
+sudo apt install eterm
+
+wget -O /tmp/bleachbit.deb https://www.bleachbit.org/download/file/t?file=bleachbit_1.12_all_ubuntu1604.deb
+dpkg -i /tmp/bleachbit.deb
+
+mkdir -p AUTOSTART_DIR
+
+wget -P AUTOSTART_DIR $GITHUB_URL/eterm.desktop
+wget -P AUTOSTART_DIR $GITHUB_URL/startup.sh
+wget -P AUTOSTART_DIR $GITHUB_URL/bleachbit.sh
+wget -P AUTOSTART_DIR $GITHUB_URL/apt.sh
