a3046d27328404a30b75907994bba1d91bfdb1a7
Eadinator <matthew@tech3.me>
2017-05-15 00:23:32 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,3 +2,6 @@
 
 bash ~/.config/autostart/bleachbit.sh
 sudo bash ~/.config/autostart/apt.sh
+
+bash ~/.config/autostart/gnome-shell-extension-installer --update 584
+gnome-shell-extension-tool -e TaskBar@zpydr
