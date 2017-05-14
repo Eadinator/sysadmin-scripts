53d6f54576ba7baafe254cb116b46930cbb1c94b
Eadinator <matthew@tech3.me>
2017-05-14 20:32:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ sudo apt install eterm
 wget -O /tmp/bleachbit.deb https://www.bleachbit.org/download/file/t?file=bleachbit_1.12_all_ubuntu1604.deb
 dpkg -i /tmp/bleachbit.deb
 
-mkdir -p AUTOSTART_DIR
+mkdir -p $AUTOSTART_DIR
 
-wget -P AUTOSTART_DIR $GITHUB_URL/eterm.desktop
-wget -P AUTOSTART_DIR $GITHUB_URL/startup.sh
-wget -P AUTOSTART_DIR $GITHUB_URL/bleachbit.sh
-wget -P AUTOSTART_DIR $GITHUB_URL/apt.sh
+wget -P $AUTOSTART_DIR $GITHUB_URL/eterm.desktop
+wget -P $AUTOSTART_DIR $GITHUB_URL/startup.sh
+wget -P $AUTOSTART_DIR $GITHUB_URL/bleachbit.sh
+wget -P $AUTOSTART_DIR $GITHUB_URL/apt.sh
