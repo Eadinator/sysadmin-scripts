bf0a25aa6584572e1392a023b95d894fc286924a
Eadinator <matthew@tech3.me>
2017-07-14 04:37:17 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,19 +10,7 @@ sudo bash ~/.config/autostart/apt.sh
 
 echo -e "\nExtensions..."
 
-echo -e "\n* Extension Update..."
-
-bash ~/.config/autostart/gnome-shell-extension-installer --update 1160 # Dash to Panel
-bash ~/.config/autostart/gnome-shell-extension-installer --update 1228 # Arc Menu
-bash ~/.config/autostart/gnome-shell-extension-installer --update 1031 # TopIcons Plus
-bash ~/.config/autostart/gnome-shell-extension-installer --update 1011 # Dynamic Panel Transparency
-
-echo -e "\n* Extension Enable..."
-
-gnome-shell-extension-tool -e dash-to-panel@jderose9.github.com
-gnome-shell-extension-tool -e arc-menu@linxgem33.com
-gnome-shell-extension-tool -e TopIcons@phocean.net
-gnome-shell-extension-tool -e dynamic-panel-transparency@rockon999.github.io
+bash ~/.config/autostart/extensions.sh
 
 echo -e "\nAll Done."
 
