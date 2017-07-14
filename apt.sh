a8f86adebd79ab246aa3e9339009cf5d69fe4543
Eadinator <matthew@tech3.me>
2017-07-14 04:30:21 +0100

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "\n* APT Upgrade..."
+echo -e "\n* Updating packages..."
 
 apt -qq update && apt --purge --yes --verbose-versions full-upgrade
 
-echo -e "\n* APT Kernel Purge..."
+echo -e "\n* Removing old kernels..."
 
 dpkg --list | awk '{print $2}' | perl ~/.config/autostart/filter_pkglist.pl $(uname -r) | xargs --no-run-if-empty apt --yes purge
