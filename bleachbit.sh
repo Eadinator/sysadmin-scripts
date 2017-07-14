fb3ed29111a1499a0a540f816b9874fd34243aef
Eadinator <matthew@tech3.me>
2017-07-14 04:33:41 +0100

    Update bleachbit.sh

--- bleachbit.sh
+++ bleachbit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "\n* Bleachbit Clean..."
+echo -e "\n* Performing bleachbit clean..."
 
 bleachbit --list-cleaners | egrep '^(google_chrome|firefox)' | xargs --no-run-if-empty bleachbit --clean >/dev/null
