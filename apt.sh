0042f3dd8736d1d2f1fc74607a71fd843c91d5cf
Eadinator <matthew@tech3.me>
2017-09-15 02:15:56 +0100

    Delete apt.sh

--- apt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "\n* Updating packages..."
-
-apt -qq update && apt --purge --yes --verbose-versions full-upgrade
-
-echo -e "\n* Removing old kernels..."
-
-dpkg --list | awk '{print $2}' | perl ~/.config/autostart/filter_pkglist.pl $(uname -r) | xargs --no-run-if-empty apt --yes purge
