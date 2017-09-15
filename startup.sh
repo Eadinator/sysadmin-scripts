8072c3e1e20d2c3a4e3a1f855aea5b092ed4f531
Eadinator <matthew@tech3.me>
2017-09-15 02:22:37 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-echo -e "\nBleachbit..."
+DIR=~/.config/autostart
 
-bash ~/.config/autostart/bleachbit.sh
+echo '+ Performing bleachbit clean...'
+bleachbit --list-cleaners | egrep '^(google_chrome|firefox)' | xargs --no-run-if-empty bleachbit --clean >/dev/null
+echo
 
-echo -e "\nAPT..."
+echo '+ Updating packages...'
+sudo apt -qq update && sudo apt --purge --yes --verbose-versions full-upgrade
+echo
 
-sudo bash ~/.config/autostart/apt.sh
+echo '+ Removing old kernels...'
+dpkg -l | awk '{print $2}' | $DIR/filter_pkglist.pl $(uname -r) | xargs --no-run-if-empty sudo apt --yes purge
+echo
 
-echo -e "\nExtensions..."
+echo '+ Updating extensions...'
+$DIR/gnome-shell-extension-installer --update 1160 # Dash to Panel
+$DIR/gnome-shell-extension-installer --update 1228 # Arc Menu
+$DIR/gnome-shell-extension-installer --update 118  # No Topleft Hot Corner
+$DIR/gnome-shell-extension-installer --update 1031 # TopIcons Plus
+echo
 
-bash ~/.config/autostart/extensions.sh
-
-echo -e "\nAll Done."
-
-sleep 3
+echo '+ Enabling extensions...'
+gnome-shell-extension-tool -e dash-to-panel@jderose9.github.com
+gnome-shell-extension-tool -e arc-menu@linxgem33.com
+gnome-shell-extension-tool -e nohotcorner@azuri.free.fr
+gnome-shell-extension-tool -e TopIcons@phocean.net
