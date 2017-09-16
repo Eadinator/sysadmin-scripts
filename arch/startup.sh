a756cd983bb5e942e6423693839581ccbb5cc970
Eadinator <matthew@tech3.me>
2017-09-16 16:21:54 +0100

    Update arch/startup.sh

--- arch/startup.sh
+++ arch/startup.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
-DIR=~/.config/autostart
+DIR=/usr/local/bin
 
-echo '+ Updating packages...'
-pacaur --noconfirm -Syu
-echo
+if [[ $EUID -eq 0 ]]; then
+        cp /etc/sudoers /etc/sudoers.bak
 
-echo '+ Installing latest kernel...'
-$DIR/kernels_to_install.pl | xargs --no-run-if-empty sudo pacman --noconfirm --needed -S
-echo
+        echo '%wheel ALL=(root) NOPASSWD: /usr/bin/pacman, /usr/bin/mv /etc/sudoers.bak /etc/sudoers' >> /etc/sudoers
 
-echo '+ Removing old kernels...'
-$DIR/kernels_to_remove.pl | xargs --no-run-if-empty sudo pacman --noconfirm -R
-echo
+        sudo -u $SUDO_USER $DIR/startup.sh
+else
+	echo '+ Updating packages...'
+	pacaur --noconfirm -Syu
+	echo
 
-echo '+ Updating extensions...'
-gnome-shell-extension-installer --update 1160 # Dash to Panel
-gnome-shell-extension-installer --update 1228 # Arc Menu
-gnome-shell-extension-installer --update 118  # No Topleft Hot Corner
-gnome-shell-extension-installer --update 1031 # TopIcons Plus
-echo
+	echo '+ Installing latest kernel...'
+	$DIR/kernels_to_install.pl | xargs --no-run-if-empty sudo pacman --noconfirm --needed -S
+	echo
 
-echo '+ Enabling extensions...'
-gnome-shell-extension-tool -e dash-to-panel@jderose9.github.com
-gnome-shell-extension-tool -e arc-menu@linxgem33.com
-gnome-shell-extension-tool -e nohotcorner@azuri.free.fr
-gnome-shell-extension-tool -e TopIcons@phocean.net
-echo
+	echo '+ Removing old kernels...'
+	$DIR/kernels_to_remove.pl | xargs --no-run-if-empty sudo pacman --noconfirm -R
+	echo
 
-echo '+ Finished +'
-sleep 3
+	echo '+ Updating extensions...'
+	gnome-shell-extension-installer --update 1160 # Dash to Panel
+	gnome-shell-extension-installer --update 1228 # Arc Menu
+	gnome-shell-extension-installer --update 118  # No Topleft Hot Corner
+	gnome-shell-extension-installer --update 1031 # TopIcons Plus
+	echo
+
+	echo '+ Enabling extensions...'
+	gnome-shell-extension-tool -e dash-to-panel@jderose9.github.com
+	gnome-shell-extension-tool -e arc-menu@linxgem33.com
+	gnome-shell-extension-tool -e nohotcorner@azuri.free.fr
+	gnome-shell-extension-tool -e TopIcons@phocean.net
+	echo
+
+        sudo mv /etc/sudoers.bak /etc/sudoers
+
+	echo '+ Finished +'
+	sleep 3
+fi
