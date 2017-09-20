a69f9b9f34c7a46a03d50645e126828c16126135
Eadinator <matthew@tech3.me>
2017-09-20 20:09:06 +0100

    Update ubuntu/startup.sh

--- ubuntu/startup.sh
+++ ubuntu/startup.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-DIR=~/.config/autostart
+DIR=/usr/local/bin
 
-echo '+ Updating packages...'
-sudo apt -qq update && sudo apt --purge --yes --verbose-versions full-upgrade
-echo
+if [[ $EUID -eq 0 ]]; then
+	echo "%sudo ALL=(root) NOPASSWD: $(which apt), $(which rm) /etc/sudoers.d/02-custom" > /etc/sudoers.d/02-custom
+	chmod 440 /etc/sudoers.d/02-custom
 
-echo '+ Removing old kernels...'
-dpkg -l | awk '{print $2}' | $DIR/filter_pkglist.pl $(uname -r) | xargs --no-run-if-empty sudo apt --yes purge
-echo
+	sudo -u $SUDO_USER $DIR/startup.sh
+else
+	echo '+ Updating packages...'
+	sudo apt -qq update && sudo apt --purge --yes --verbose-versions full-upgrade
+	echo
 
-echo '+ Updating extensions...'
-$DIR/gnome-shell-extension-installer --update 1160 # Dash to Panel
-$DIR/gnome-shell-extension-installer --update 1228 # Arc Menu
-$DIR/gnome-shell-extension-installer --update 118  # No Topleft Hot Corner
-$DIR/gnome-shell-extension-installer --update 1031 # TopIcons Plus
-echo
+	echo '+ Removing old kernels...'
+	dpkg -l | awk '{print $2}' | $DIR/filter_pkglist.pl $(uname -r) | xargs --no-run-if-empty sudo apt --yes purge
+	echo
 
-echo '+ Enabling extensions...'
-gnome-shell-extension-tool -e dash-to-panel@jderose9.github.com
-gnome-shell-extension-tool -e arc-menu@linxgem33.com
-gnome-shell-extension-tool -e nohotcorner@azuri.free.fr
-gnome-shell-extension-tool -e TopIcons@phocean.net
-echo
+	echo '+ Updating extensions...'
+	gnome-shell-extension-installer --update 1160 # Dash to Panel
+	gnome-shell-extension-installer --update 1228 # Arc Menu
+	gnome-shell-extension-installer --update 118  # No Topleft Hot Corner
+	gnome-shell-extension-installer --update 1031 # TopIcons Plus
+	echo
 
-echo '+ Finished +'
-sleep 3
+	echo '+ Enabling extensions...'
+	gnome-shell-extension-tool -e dash-to-panel@jderose9.github.com
+	gnome-shell-extension-tool -e arc-menu@linxgem33.com
+	gnome-shell-extension-tool -e nohotcorner@azuri.free.fr
+	gnome-shell-extension-tool -e TopIcons@phocean.net
+	echo
+
+	sudo rm /etc/sudoers.d/02-custom
+
+	echo '+ Finished +'
+	sleep 3
+fi
